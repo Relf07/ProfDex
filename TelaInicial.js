46bd9fbb351a59eedebcb0a87e9397b0fef02a93
Felipe Rodrigues Pereira <106030300+Relf07@users.noreply.github.com>
2024-04-16 07:04:46 -0300

    Add files via upload

---
+++ TelaInicial.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button, ImageBackground, StyleSheet, View } from 'react-native';
+
+const TelaInicial = ({ navigation }) => {
+  const irParaOutraTela = () => {
+    navigation.navigate('SegundaTela');
+  };
+
+  return (
+    <ImageBackground
+      source={require('./src/background/Poke.jpg')}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Button title="Ir para outra tela" onPress={irParaOutraTela} />
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default TelaInicial;
